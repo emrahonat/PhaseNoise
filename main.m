1c1ab4225ecaac89d366264e618a610d938e0051
emrah onat <eonat87@yahoo.com>
2025-10-02 00:12:17 +0200

    first commit

---
+++ main.m
@@ -0,0 +1,11 @@
+%% Phase Noise Effects on Advanced Radar Waveforms
+%
+% Please cite these 2 articles
+% 
+% [1] E. Onat and A. Orduyilmaz, "The Effect of Phase Noise on Radar Waveforms and Performance Analysis," 2025 33rd Signal Processing and Communications Applications Conference (SIU), Sile, Istanbul, Turkiye, 2025, pp. 1-4, doi: 10.1109/SIU66497.2025.11111835.
+% [2] E. Onat and A. Orduyilmaz, "Phase Noise Effects on Advanced Radar Waveforms: A Performance Study of Linear and Nonlinear Frequency Modulations," International Journal of Advances in Engineering and Pure Sciences, vol. XX, no. Y, pp. AAA–BBB, 202C, doi: 10.7240/jeps.1111111.
+%
+
+% 
+% Dr. Emrah Onat (eonat87@yahoo.com)
+% Dr. Adnan Orduyilmaz (orduyilmaz@gmail.com)
